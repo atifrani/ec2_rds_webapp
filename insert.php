ecc66625db5a7d0b3c91a4b0250d7b9b2ccbe083
atifrani <52487696+atifrani@users.noreply.github.com>
2026-04-03 12:17:20 +0200

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,28 +1,57 @@
 <!DOCTYPE html>
+<html>
 <head>
-<title>Insert data to PostgreSQL with php - creating a simple web application</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Insert data to PostgreSQL with PHP</title>
+<meta charset="utf-8" />
 <style>
-li {listt-style: none;}
+li { list-style: none; }
 </style>
 </head>
 <body>
+
 <h2>Enter information regarding new employee</h2>
+
+<form name="insert" action="" method="POST">
 <ul>
-<form name="insert" action="insert.php" method="POST" >
 <li>Employee ID:</li><li><input type="text" name="id" /></li>
 <li>Employee FirstName:</li><li><input type="text" name="fname" /></li>
 <li>Employee LastName:</li><li><input type="text" name="lname" /></li>
 <li>Employee Gender:</li><li><input type="text" name="gender" /></li>
 <li>Employee Age:</li><li><input type="text" name="age" /></li>
 <li>Employee Location:</li><li><input type="text" name="location" /></li>
-<li><input type="submit" /></li>
-</form>
+<li><input type="submit" name="submit" /></li>
 </ul>
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    $db = pg_connect("host=database-1.cwzeerf2ugo3.eu-west-1.rds.amazonaws.com port=5432 dbname=postgres user=postgres password=admin_12345");
+
+    if (!$db) {
+        die("Connection failed");
+    }
+
+    // Use parameterized query (SAFE)
+    $query = "INSERT INTO employee (id, fname, lname, gender, age, location)
+              VALUES ($1, $2, $3, $4, $5, $6)";
+
+    $result = pg_query_params($db, $query, [
+        $_POST['id'],
+        $_POST['fname'],
+        $_POST['lname'],
+        $_POST['gender'],
+        $_POST['age'],
+        $_POST['location']
+    ]);
+
+    if ($result) {
+        echo "Data inserted successfully.";
+    } else {
+        echo "Error inserting data.";
+    }
+}
+?>
+
 </body>
 </html>
-<?php
-$db = pg_connect("host=database-1.cwzeerf2ugo3.eu-west-1.rds.amazonaws.com port=5432 dbname=postgres user=postrges password=admin_12345");
-$query = "INSERT INTO employee VALUES ('$_POST[id]','$_POST[fname]', '$_POST[lname]','$_POST[gender]','$_POST[age]', '$_POST[location]')";
-$result = pg_query($query); 
-?>
