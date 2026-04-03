235dbd75cbc50d5d2ba97c4cf57ff701395fc7a0
atifrani <52487696+atifrani@users.noreply.github.com>
2026-04-03 12:06:29 +0200

    Update find.php

--- find.php
+++ find.php
@@ -1,47 +1,55 @@
 <!DOCTYPE html>
- <head>  <title>Enter bookid to display data - creating a simple web application</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <style>
-li {list-style: none;}
-</style>
+<html>
+<head>
+    <title>Find employee by ID</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        li { list-style: none; }
+    </style>
 </head>
 <body>
-<h2>Enter Employee ID and enter</h2>
-<ul>
-<form name="find" action="find.php" method="POST" >
-<li>Employee ID:</li><li><input type="text" name="id" /></li>
-<li><input type="submit" name="submit" /></li>
-</form>
-</ul>
-</body>
-</html>
+    <h2>Enter Employee ID and press submit</h2>
+
+    <form name="find" action="" method="POST">
+        <ul>
+            <li>Employee ID:</li>
+            <li><input type="text" name="id" /></li>
+            <li><input type="submit" name="submit" value="Search" /></li>
+        </ul>
+    </form>
+
 <?php
-if (isset($_POST['submit']))
-{
-$db = pg_connect("host=database-1.cwzeerf2ugo3.eu-west-1.rds.amazonaws.com port=5432 dbname=postgres user=postrges password=admin_12345");
-$query = "select * from employee where id = '$_POST[id]'";
-$result = pg_query($db, $query);
-if (!$result)
-{
-echo "Please enter an existing employee id!!";
-} else
-{
-while ($row = pg_fetch_assoc($result)){
-echo "<tr>";
-echo "<td> Employee ID: ",$row[id], "</td>";
-echo "<br>";
-echo "<td>Employee FirstName: ",$row[fname], "</td>";
-echo "<br>";
-echo "<td>Employee LastName: ",$row[lname], "</td>";
-echo "<br>";
-echo "<td>Employee Gender: ",$row[gender], "</td>";
-echo "<br>";
-echo "<td>Employee Age: ",$row[age], "</td>";
-echo "<br>";
-echo "<td>Employee Location: ",$row[location], "</td>";
-echo "<br>";
-echo "</tr>";
-}
-}
+if (isset($_POST['submit'])) {
+
+    $db = pg_connect("host=database-1.cwzeerf2ugo3.eu-west-1.rds.amazonaws.com port=5432 dbname=postgres user=postgres password=admin_12345");
+
+    if (!$db) {
+        die("Database connection failed.");
+    }
+
+    $query = "SELECT * FROM employee WHERE id = $1";
+    $result = pg_query_params($db, $query, array($_POST['id']));
+
+    if (!$result) {
+        echo "<p>Query failed.</p>";
+    } else {
+        if (pg_num_rows($result) == 0) {
+            echo "<p>Please enter an existing employee id.</p>";
+        } else {
+            while ($row = pg_fetch_assoc($result)) {
+                echo "<p><strong>Employee ID:</strong> " . htmlspecialchars($row['id']) . "</p>";
+                echo "<p><strong>Employee FirstName:</strong> " . htmlspecialchars($row['fname']) . "</p>";
+                echo "<p><strong>Employee LastName:</strong> " . htmlspecialchars($row['lname']) . "</p>";
+                echo "<p><strong>Employee Gender:</strong> " . htmlspecialchars($row['gender']) . "</p>";
+                echo "<p><strong>Employee Age:</strong> " . htmlspecialchars($row['age']) . "</p>";
+                echo "<p><strong>Employee Location:</strong> " . htmlspecialchars($row['location']) . "</p>";
+            }
+        }
+    }
+
+    pg_close($db);
 }
 ?>
+
+</body>
+</html>
